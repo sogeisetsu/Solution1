fa9ae96bd8b36dfb5650badf0fa9e19747580f32
suyuesheng <suyuesheng01@gmail.com>
2021-10-30 19:44:47 +0800

    添加项目文件。

---
+++ Format/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Format
+{
+    public class FormatOne
+    {
+        public FormatOne() {
+            Console.WriteLine("字符串格式化");
+        }
+        /// <summary>
+        /// 字符串格式化
+        /// </summary>
+        public static void Format()
+        {
+            string a = "hello world";
+            string c = "hello world";
+            string b = a.Substring(0, 1).ToUpper() + a.Substring(1);
+            Console.WriteLine($"首字母大写\t{b}");
+            Console.WriteLine("比较");
+            Console.WriteLine(a.Equals("hello world"));
+            Console.WriteLine(a.CompareTo("hello world"));
+            Console.WriteLine(a == c);
+            Console.WriteLine(a.GetType());
+            Type type = typeof(int);
+            Console.WriteLine(type.GetType());
+            //两种字符转内插
+            Console.WriteLine($"{a}\t{b}\t{c}");
+            Console.WriteLine("@可以无视转义符");
+            Console.WriteLine("\\a\\f");
+            Console.WriteLine(@"\\a\\f");
+        }
+    }
+}
---
+++ ShowCase/Program.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary1;
+using System;
+using Format;
+namespace ShowCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            new LibraryOne().One("你好");
+            Console.WriteLine("按下任何键去停止并退出程序");
+            Console.ReadKey(true);
+            FormatOne.Format();
+        }
+    }
+}
