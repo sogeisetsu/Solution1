233c0e015675f96bdc6662d1fcad1b2dff8bbb72
suyuesheng <suyuesheng01@gmail.com>
2021-10-31 17:50:53 +0800

    数据类型转换

--- ShowCase/AsIs.cs
+++ ShowCase/AsIs.cs
@@ -4,6 +4,10 @@ namespace ShowCase
 {
     class AsIs
     {
+        /// <summary>
+        /// 常量，除了不可更改，只能用调用静态变量的方式调用
+        /// </summary>
+        const int a = 1;
         //object a = "123";
         public void One(object c)
         {
@@ -32,5 +36,39 @@ namespace ShowCase
 
             // 类型
         }
+        public void Two()
+        {
+            Console.WriteLine(a);
+        }
+        public static void TwoStatic()
+        {
+            Console.WriteLine(a);
+
+        }
+        /// <summary>
+        /// 数据类型转换
+        /// </summary>
+        void ShuJvLeiXingZhuanHuan()
+        {
+            // 隐式数据转换
+            int sa = 1;
+            double sd = sa;
+            Console.WriteLine(sd);
+            // 显示数据类型转换
+            double sdd = 12.435;
+            // 转换为字符串
+            Console.WriteLine(sdd.ToString());
+            // 数字转换
+            Console.WriteLine(((int)sdd).GetType());
+            // Parse
+            string dp = "12.5";
+            double.Parse(dp);
+            // Convert 
+            Convert.ToInt32(sdd);
+
+        }
+
+
+
     }
 }
