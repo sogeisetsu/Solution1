770bd6b204c0bfa1a84d347d0ea45cdbede235c6
suyuesheng <suyuesheng01@gmail.com>
2021-11-07 12:45:15 +0800

    运算符和类

--- ShowCase/AsIs.cs
+++ ShowCase/AsIs.cs
@@ -4,6 +4,9 @@ using System.Reflection;
 
 namespace ShowCase
 {
+    /// <summary>
+    /// 数据类型转换
+    /// </summary>
     internal class AsIs
     {
         /// <summary>
@@ -90,8 +93,64 @@ namespace ShowCase
         }
     }
 
+    internal class Book
+    {
+        public Book()
+        {
+        }
+
+        public Book(string name) => Name = name ?? throw new ArgumentNullException(nameof(name));
+
+        public Book(string name, double price)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Price = price;
+        }
+
+        /// <summary>
+        /// init only setter,只能在构造函数中被设置，在其余时间为只读属性
+        /// </summary>
+        public int InitC { get; init; } = 12;
+
+        public string Name { get; set; } = "老人与海";
+
+        public double Price { get; set; } = 13.46;
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            //var book = new Book();
+            //var type = book.GetType();
+            //PropertyInfo[] propertyInfos = type.GetProperties();
+            //foreach (var item in propertyInfos)
+            //{
+            //    //Console.WriteLine(item.GetType());
+            //    Console.WriteLine(item.GetValue(book));
+            //}
+            return $"Name\t{Name}\nPrice\t{Price}\nInitC\t{InitC}";
+            //return base.ToString();
+        }
+    }
+
+    /// <summary>
+    /// 运算符
+    /// </summary>
     internal class Operator
     {
+        /// <summary>
+        /// lambda的委托方法
+        /// </summary>
+        /// <returns>平方</returns>
+        public Func<int, int> IntPow = x => x * x;
+
         internal readonly int? dd;
 
         /// <summary>
@@ -107,51 +166,17 @@ namespace ShowCase
         public int LambdaTry(int x) => x * x;
 
         /// <summary>
-        /// is as
-        /// </summary>
-        /// <param name="c"></param>
-        public void One(object c)
-        {
-            string cc = c as string;
-            int? cd;
-            try
-            {
-                cd = c as int?;
-            }
-            catch (Exception e)
-            {
-                cd = null;
-                Console.WriteLine(e);
-            }
-            // string
-            Console.WriteLine("c as string\t{0}", c as string);
-            Console.WriteLine(cc.GetType());
-            Console.WriteLine("c is string\t{0}", c is string);
-            Console.WriteLine((c is string).GetType());
-
-            // int
-            Console.WriteLine("c as int?\t{0}", c as int?);
-            Console.WriteLine(cd == null);
-            Console.WriteLine("c is int\t{0}", c is int);
-            Console.WriteLine((c is int).GetType());
-            /*
-                c as string     123
-                System.String
-                c is string     True
-                System.Boolean
-                c as int?
-                True
-                c is int        False
-                System.Boolean*/
-
-            // 类型
-        }
-
-        /// <summary>
         /// ?? 和？
         /// </summary>
         public void QuesttionMode()
         {
+            // ??原理
+            #region
+            /*
+             如果左操作数的值不为 null，则 null 合并运算符 ?? 返回该值；否则，它会计算右操作数并返回其结果。
+            ?的意思是可以为null
+             */
+            #endregion
             int? c = null; //默认值是null
             int? d = 12;
 
@@ -159,6 +184,54 @@ namespace ShowCase
             Console.WriteLine(d ?? 34);
             Console.WriteLine(c ??= 12);
             Console.WriteLine(dd);
+            // 运算结果
+            #region
+            /*
+             15
+             12
+             12
+
+            */
+            #endregion
+        }
+
+        /// <summary>
+        /// is 和 as 的区别辨析
+        /// </summary>
+        internal void IsAs()
+        {
+            //as 和 is的就是返回值的区别,is返回true或者false，os如果符合就直接返回结果。不符合就返回null
+            object demoString = "Hello world";
+            object demoStringInt = "123";
+            object demoInt = 124;
+            int? @null = null;
+            Console.WriteLine($"demoString is string\t{demoString is string}");
+            Console.WriteLine($"demoStringInt is int\t{demoStringInt is int}");
+            Console.WriteLine($"demoInt is demoInt\t{demoInt is int}");
+            try
+            #region
+            {
+                string ASDemoString = demoString as string;
+                Console.WriteLine(ASDemoString.GetType());
+                Console.WriteLine(demoString as string);
+                Console.WriteLine(demoStringInt as int? ?? throw new ArgumentNullException(nameof(demoStringInt)));
+            }
+            #endregion
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            //运行结果
+            #region
+            /*
+             demoString is string    True
+             demoStringInt is int    False
+             demoInt is demoInt      True
+             Hello world
+             System.ArgumentNullException: Value cannot be null. (Parameter 'demoStringInt')
+                 at ShowCase.Operator.IsAs() in C:\Users\苏月晟\source\repos\Solution1\ShowCase\AsIs.cs:line 213
+             */
+            #endregion
         }
 
         /// <summary>
@@ -178,6 +251,9 @@ namespace ShowCase
         }
     }
 
+    /// <summary>
+    /// 属性，get和set
+    /// </summary>
     internal class PropertyTry
     {
         public PropertyTry()
@@ -201,6 +277,67 @@ namespace ShowCase
         public override string ToString()
         {
             return "测试属性和变量";
+        }
+
+        internal void ReClass()
+        {
+            var book = new Book();
+            Console.WriteLine(book.GetType());
+            Console.WriteLine(book);
+        }
+    }
+
+    /// <summary>
+    /// 检验效果
+    /// </summary>
+    internal class Tset
+    {
+        public Tset()
+        {
+            One();
+            Console.WriteLine("------------------------");
+            Two();
+            Console.WriteLine("-------------------------");
+            Console.WriteLine(nameof(Three));
+            Three();
+
+        }
+
+        /// <summary>
+        /// 通过构造函数来访问
+        /// </summary>
+        internal void One()
+        {
+            Book book = new Book("123", 12.4);
+            Console.WriteLine(book);
+        }
+
+        /// <summary>
+        /// 调用 init only setter
+        /// </summary>
+        internal void Three()
+        {
+            Book book = new Book()
+            {
+                // InitC 只能从构造函数或者启动器里面设置，因为他是init only setter
+                InitC = 124,
+                Name = "1223",
+                Price = 13.456
+            };
+            Console.WriteLine(book);
+        }
+
+        /// <summary>
+        /// 通过初始化器来创造实例
+        /// </summary>
+        internal void Two()
+        {
+            Book book = new Book()
+            {
+                Name = "123",
+                Price = 12.45
+            };
+            Console.WriteLine(book);
         }
     }
 }
--- ShowCase/Program.cs
+++ ShowCase/Program.cs
@@ -6,26 +6,35 @@ namespace ShowCase
     {
         private static void Main(string[] args)
         {
+            #region 
             //Console.WriteLine("Hello World!");
             //new LibraryOne().One("你好");
             //new Class1();
             //FormatOne.Format();
             //new ClassCd();
-            object c = "123";
-            new AsIs().One(c);
-            new AsIs().Two();
-            try
-            {
-                object cc = null;
-                Console.WriteLine(cc ?? throw new ArgumentNullException(nameof(cc)));
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-            new AsIs().RefOut();
-            new Operator().QuesttionMode();
-            Console.WriteLine(new Operator().dd);
+            //object c = "123";
+            //new AsIs().One(c);
+            //new AsIs().Two();
+            //try
+            //{
+            //    object cc = null;
+            //    Console.WriteLine(cc ?? throw new ArgumentNullException(nameof(cc)));
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e);
+            //}
+            //new AsIs().RefOut();
+            //new Operator().QuesttionMode();
+            //Operator @operator = new Operator();
+            //@operator.IsAs();
+            //Console.WriteLine(new Operator().dd);
+            //int? cc = null;
+            //Console.WriteLine(cc == null ? "null" : cc);
+            //Console.WriteLine("+++++++++++++++++++++++++");
+            //new PropertyTry().ReClass();
+            #endregion
+            new Tset();
             Console.WriteLine("按下任何键去停止并退出程序");
             Console.ReadKey(true);
         }
